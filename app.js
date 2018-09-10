acec6c84c8cdc3e6c27d352e62387d6ad9d988a1
Mitsuteru Sawa <mttrs@users.noreply.github.com>
2018-09-10 12:33:05 +0900

    hi, there

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-  res.send('Workout! - master tag1');
+  res.send('Hi there');
 });
 
 app.listen(app.get('port'), function() {
